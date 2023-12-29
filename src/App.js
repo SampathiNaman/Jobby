ae7a6a2edfc1ccc08ba927680f4f05d59a56748d
Naman Narayana Swamy <96163937+SampathiNaman@users.noreply.github.com>
2023-12-29 13:23:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ const App = () => (
       <ProtectedRoute exact path="/" component={Home} />
       <ProtectedRoute exact path="/jobs" component={Jobs} />
       <ProtectedRoute exact path="/jobs/:id" component={JobItem} />
-      <Route component={NotFound} />
+<Route exact path="/not-found" component={NotFound} />      
+<Redirect to="not-found" />
     </Switch>
   </div>
 )
